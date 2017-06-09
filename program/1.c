a58342c92b567902c11279908bc614d09df7d125
AnnapooraniChandran <annapoorani1557@gmail.com>
2017-06-09 09:23:51 +0530

    Create 1.c

---
+++ program/1.c
@@ -0,0 +1,14 @@
+#includce<stdio.h>
+int main(){
+ int input;
+ scanf("%d",&input);
+ if(input>0){
+  printf("positive\n");
+  }
+  else if(input<0){
+   printf("negative\n");
+  }
+  else{
+   printf("zero\n");
+  }
+ }
